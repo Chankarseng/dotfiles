1d20da8432c766eb902a4bb2aff413a5b3e24d89
Chan Kar Seng <karseng@snappymob.com>
2025-08-07 19:45:43 +0800

    feat: update lspconfig for vue

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,21 @@
+-- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
+require("lspconfig").ts_ls.setup {
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vim.fn.stdpath "data" .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+        languages = { "vue" },
+      },
+    },
+  },
+}
+
 local servers = {
+  "vue_ls",
   "html",
   "cssls",
   "ts_ls",
@@ -9,7 +24,6 @@ local servers = {
   "pyright",
   "prismals",
   "terraformls",
-  "vue_ls",
   "intelephense",
   "tailwindcss",
   "yamlls",
