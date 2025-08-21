2a34f2ee54d9e37a609ea6601ac9a12fe9047cf9
Chan Kar Seng <chankarseng96@gmail.com>
2025-08-22 00:53:00 +0800

    fix: install script

--- install.sh
+++ install.sh
@@ -3,14 +3,13 @@
 # nvim
 # backup existing config if config already exist
 if [ -d ~/.config/nvim ]; then
-  mv ~/.config/nvim ~/.config/nvim.bak
+  mv ~/.config/nvim ~/.config/nvim.bak.$(date +%Y%m%d%H%M%S)
 fi
-mkdir -p ~/.config/nvim
-cp nvim ~/.config/nvim
+cp -r nvim ~/.config/nvim
 
 # tmux
 # backup existing config if config already exist
 if [ -f ~/.tmux.conf ]; then
-  mv ~/.tmux.conf ~/.tmux.conf.bak
+  mv ~/.tmux.conf ~/.tmux.conf.bak.$(date +%Y%m%d%H%M%S)
 fi
-cp tmux.conf ~/.tmux.conf
+cp .tmux.conf ~/.tmux.conf
