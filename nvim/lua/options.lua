28e0ef48f765a00e505d6c5369b7d0381c47d7ca
Chan Kar Seng <karseng@snappymob.com>
2026-08-10 09:53:50 +0800

    nvim/options: remove list

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -43,8 +43,8 @@ vim.o.timeoutlen = 300
 vim.o.splitright = true
 vim.o.splitbelow = true
 
-vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.o.list = false
+vim.opt.listchars = { tab = '  ', trail = '·', nbsp = '␣' }
 
 vim.o.inccommand = 'split'
 
@@ -56,9 +56,8 @@ vim.o.scrolloff = 10
 vim.o.confirm = true
 
 -- Fold options
-vim.o.foldcolumn = "0"
+vim.o.foldcolumn = '0'
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:▾,foldsep: ,foldclose:▸]]
-
